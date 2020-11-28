a64d70fd5b5ac9aa10adaa0f50544e43190a0c4b
Vlad <74093196+Fanz11@users.noreply.github.com>
2020-11-28 10:07:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
+from datetime import datetime
 # первое
-my_list = [1, 2, 3]
-my_new_list = [i * 2 for i in my_list]
-print(my_new_list)
+number1 = [1, 2, 3]                                 #числа
+number2 = [i * 2 for i in number1]                  #умножение на 2
+print(number2)                                      #ответ
 
 
 # второе
-my_list = [1, 2, 3]
-my_new_list = [i ** 2 for i in my_list]
-print(my_new_list)
+number1 = [1, 2, 3]                                #числа
+number2 = [i ** 2 for i in number1]                #возведение в вторую степень
+print(number2)                                     #ответ
+
+#трертье
+list = 'Hello world'
+if " " in list:                                    #если есть пробел
+    list = list.upper()                            #перевод в верхний регистр
+    print(list)                                    #ответ
+else:                                              #если нет пробела
+    list = list.lower()                            #то переводиться в нижний регистр
+    print(list)                                    #ответ
 
 
 
 # четвертое
-from datetime import datetime
-year1 = datetime.now().year
+year1 = datetime.now().year                       
 year2 = 1900
-rad = year1 - year2
-for i in range(rad+1):
-    print(year2 + i)
+answer = year1 - year2
+for i in range(answer + 1):
+        print(year2 + i)
